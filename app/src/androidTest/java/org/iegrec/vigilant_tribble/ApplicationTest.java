67b41bbc3a4fb6a51cf767d2ef7cad4962dca7b4
Joe Moore <jpvlsmv@gmail.com>
2016-08-10 19:00:27 -0400

    Initial activity from wizard

---
+++ app/src/androidTest/java/org/iegrec/vigilant_tribble/ApplicationTest.java
@@ -0,0 +1,13 @@
+package org.iegrec.vigilant_tribble;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
